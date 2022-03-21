16fe911c6a151b3239afbc727292eb46ca480b68
Boost <boostbm@gmail.com>
2022-03-21 16:56:21 -0700

    Updated JS

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// ADD SELECTOR
-document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {  
+// Selector 
 
-  if(question.parentNode.classList.contains("active")) {
-      question.parentNode.classList.toggle("active")
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => question.addEventListener('click', () => {
+  if(question.parentNode.classList.contains('active')) {
+    question.parentNode.classList.toggle('active'); 
   }
   else {
-    document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
-
-    question.parentNode.classList.add("active")
-  }   
+    questions.forEach((question) => question.parentNode.classList.remove('active'));
+  }
 }))
+
+console.log(questions);
